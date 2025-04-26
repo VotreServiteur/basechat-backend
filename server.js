171325fe176d767a404eced304a0533a12ef1d39
Maksim Budrevich <maksimbudrevich@gmail.com>
2025-04-26 13:21:37 +0300

    feat: Add register endpoint

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { Pool } = require('pg');
 const app = express();
 const port = process.env.PORT || 3001;
 
-
+const bcrypt = require('bcryptjs');
 
 app.use(express.json());
 
@@ -34,3 +34,47 @@ app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
 
+app.post('/api/auth/register', async (req, res) => {
+    const { login, password } = req.body;
+
+    if (!login || !password) {
+        return res.status(400).json({
+            success: false,
+            message: 'Login and password are required.'
+        });
+    }
+
+    try {
+        const userExists = await pool.query(
+            'SELECT 1 FROM users WHERE login = $1',
+            [login]);
+
+        if (userExists.rows.length > 0) {
+            return res.status(409).json({
+                success: false,
+                message: 'Login already exists'
+            });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await pool.query(
+            'INSERT INTO users (login, password) VALUES ($1, $2) RETURNING id, login',
+            [login, hashedPassword]
+        );
+
+        res.status(201).json({
+            success: true,
+            message: 'User registered successfully',
+            user: newUser.rows[0]
+        });
+    } catch (err) {
+        console.error('Error during registration:', err.stack);
+        res.status(500).json({
+            success: false,
+            message: 'Registration failed'
+        })
+    }
+
+})
