57b4d126e9cc22b19a361098e02670af0abbda9b
Maksim Budrevich <maksimbudrevich@gmail.com>
2025-04-27 21:13:57 +0300

    feat: Add messages sending and receiving

--- server.js
+++ server.js
@@ -29,18 +29,18 @@ const pool = new Pool({
     port: process.env.DB_PORT,
 });
 
-function authenticateToken(req, res, next){
+function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
     if (token == null) {
-        return res.status(401).json({success: false, message: 'Authentication token required/'});
+        return res.status(401).json({ success: false, message: 'Authentication token required/' });
     }
 
 
     jwt.verify(token, jwtSecret, (err, user) => {
-        if(err){
-            return res.status(403).json({ 
+        if (err) {
+            return res.status(403).json({
                 success: false,
                 message: 'Invalid token'
             });
@@ -51,22 +51,6 @@ function authenticateToken(req, res, next){
     });
 }
 
-
-pool.connect((err, client, release) => {
-    if (err) {
-        return console.error('Error acquiring client', err.stack);
-    }
-    console.log('Connected to PostgreSQL database')
-    release();
-});
-
-app.get('/', (req, res) => {
-    res.send('Back is running');
-})
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
 
 app.post('/api/auth/register', async (req, res) => {
     const { login, password } = req.body;
@@ -114,7 +98,7 @@ app.post('/api/auth/register', async (req, res) => {
 })
 
 app.post('/api/auth/login', async (req, res) => {
-    const {login, password} = req.body;
+    const { login, password } = req.body;
     if (!login || !password) {
         return res.status(400).json({
             success: false,
@@ -127,18 +111,18 @@ app.post('/api/auth/login', async (req, res) => {
             'SELECT * FROM users WHERE login = $1',
             [login]);
 
-        if (userResult.rows.length === 0 ) {
+        if (userResult.rows.length === 0) {
             return res.status(401).json({
                 success: false,
                 message: 'Invalid login or password.'
             })
         }
-        
+
         const user = userResult.rows[0];
 
         const isPasswordValid = await bcrypt.compare(password, user.password);
-        
-        if (!isPasswordValid){
+
+        if (!isPasswordValid) {
             return res.status(401).json({
                 success: false,
                 message: 'Invalid login or password.'
@@ -146,9 +130,9 @@ app.post('/api/auth/login', async (req, res) => {
         }
 
         const token = jwt.sign(
-            {userId: user.id, login: user.login},
+            { userId: user.id, login: user.login },
             jwtSecret,
-            {expiresIn: '1h'}
+            { expiresIn: '1h' }
         )
 
         res.status(200).json({
@@ -161,9 +145,89 @@ app.post('/api/auth/login', async (req, res) => {
             }
         });
 
-    } catch(err){
+    } catch (err) {
         console.error('Error during login:', err.stack);
         res.status(500).json({ success: false, message: 'Login failed' });
     }
 })
 
+app.get(
+    '/api/messages',
+    authenticateToken,
+    async (req, res) => {
+        try {
+            const result = await pool.query(`
+                SELECT
+                    m.id,
+                    m.text,
+                    m.sender_id,
+                    u.login as sender_login,
+                    m.created_at
+                FROM messages m
+                JOIN users u ON m.sender_id = u.id
+                ORDER BY m.created_at ASC
+                `);
+
+            res.status(200).json(result.rows);
+
+        } catch (err) {
+            console.error('Error fetching messages:', err.stack);
+            res.status(500).json({ success: false, message: 'Failed to fetch messages' });
+
+        }
+});
+
+app.post('/api/messages', authenticateToken, async (req, res) => {
+    const senderId = req.user.userId;
+    const { text } = req.body;
+
+    if (!text || text.trim().length === 0){
+        return res.status(400).json({ success: false, message: 'message text can not be empty'});
+    }
+    const chatId = 1;
+    recieving
+
+    try {
+        const result = await pool.query(
+            'INSERT INTO messages (chat_id, sender_id, text) VALUES ($1,$2,$3) RETURNING id, chat_id, sender_id, text, created_at',
+            [chatId, senderId, text]
+        );
+        const newMessage = result.rows[0];
+
+        const senderResult = await pool.query('SELECT login FROM users WHERE id = $1', [senderId]);
+        const senderLogin = senderResult.rows[0].login;
+
+        res.status(201).json({
+            success: true,
+            message: 'Message sent successfully',
+            messageData: {
+                id: newMessage.id,
+                chat_id: newMessage.chat_id,
+                sender_id: newMessage.sender_id,
+                text: newMessage.text,
+                created_at: newMessage.created_at,
+                sender_login: senderLogin
+            }
+        })
+
+    }catch (err){
+        console.error('Error sending message:', err.stack);
+        res.status(500).json({ success: false, message: 'Failed to send message'});
+    }
+});
+
+pool.connect((err, client, release) => {
+    if (err) {
+        return console.error('Error acquiring client', err.stack);
+    }
+    console.log('Connected to PostgreSQL database')
+    release();
+});
+
+app.get('/', (req, res) => {
+    res.send('Back is running');
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
